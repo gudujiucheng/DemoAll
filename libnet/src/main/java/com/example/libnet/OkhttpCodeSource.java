17bb05fe4735ca3f639a34805cc56f1fa32423d1
Administrator <123456>
2018-09-08 15:42:32 +0800

    结构整理

---
+++ libnet/src/main/java/com/example/libnet/OkhttpCodeSource.java
@@ -0,0 +1,5 @@
+package com.example.libnet;
+
+public class OkhttpCodeSource {
+
+}
---
+++ myview/src/main/java/com/example/myview/ChannelView/OnItemMoveListener.java
@@ -0,0 +1,8 @@
+package com.example.myview.ChannelView;
+/**
+ * Item移动后 触发
+ * Created by YoKeyword on 15/12/28.
+ */
+public interface OnItemMoveListener {
+    void onItemMove(int fromPosition, int toPosition);
+}
