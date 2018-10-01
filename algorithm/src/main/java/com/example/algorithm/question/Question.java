56c9767516e0a2c74df043deb9f6c67ce38735df
zhangcan603 <qweasd##123>
2018-10-01 16:52:09 +0800

    排序

--- algorithm/src/main/java/com/example/algorithm/question/Question.java
+++ algorithm/src/main/java/com/example/algorithm/question/Question.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Question {
 
     public static void main(String[] args) {
-        test();
+        test01();
     }
 
 
@@ -26,18 +26,29 @@ public class Question {
 
 /*    有一个整形数组，包含正数和负数，然后要求把数组内的所有负数移至正数的左边，且保证相对位置不变，
     要求时间复杂度为O(n), 空间复杂度为O(1)。
-    例如，{10, -2, 5, 8, -4, 2, -3, 7, 12, -88, -23, 35}变化后是{-2, -4，-3, -88, -23,5, 8 ,10, 2, 7, 12, 35}。*/
+    例如，{10, -2, 5, 8, -4, 2, -3, 7, 12, -88, -23, 35}变化后是{-2, -4，-3, -88, -23,5, 8 ,10, 2, 7, 12, 35}。
 
-private static void  test01(int[] aar){//TODO  未完待续
-    int tempPosition = -1;
+    参考：https://blog.csdn.net/epubit17/article/details/80342004
+
+    */
+
+private static void  test01(){//TODO  未完待续
+    int[] aar = new int[]{10, -2, 5, 8, -4, 2, -3, 7, 12, -88, -23, 35};
+    int tempPosition = -1;//记录最左边负数在数组中的索引值   默认第一次是第0个 也就是  -1+1
+    int tempValue;
 
     for (int i = 0; i <aar.length ; i++) {
-        int x= aar[i];
-        if(x<0&&i!=0){
-            tempPosition = i;
-
+        if(aar[i]<0){
+            tempValue = aar[tempPosition+1];
+            aar[tempPosition+1] = aar[i];
+            aar[i] = tempValue;
+            tempPosition++;//需要向前移动一位
         }
 
+    }
+
+    for (int i = 0; i < aar.length; i++) {
+        System.out.print(" "+aar[i]);
     }
 
 
