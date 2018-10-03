e2c0d0ff5ab29b79c3fe44988673037f9c008242
zhangcan603 <qweasd##123>
2018-10-03 16:25:40 +0800

    约瑟夫环问题

--- algorithm/src/main/java/com/example/algorithm/question/Question.java
+++ algorithm/src/main/java/com/example/algorithm/question/Question.java
@@ -16,7 +16,6 @@ public class Question {
     private static void test() {
 //        打印不大于n的最大素数_.printMaxPrime(1000);
 //        找出两个相同的数字_.printSameNum(new int[]{1,5,3,234,343,54456,565,23,4,234});
-        报数_.getTheLastOne(new ArrayList<Integer>(){{add(1);add(3);add(5);add(7);add(9);}},3);
     }
 
 
--- algorithm/src/main/java/com/example/algorithm/question/报数_.java
+++ algorithm/src/main/java/com/example/algorithm/question/报数_.java
@@ -1,32 +1,74 @@
 package com.example.algorithm.question;
 
-import java.util.ArrayList;
-import java.util.List;
+
+/**
+ *  参考 https://blog.csdn.net/mark555/article/details/22402705
+ *  约瑟夫环问题（还有一种找规律的公式解法更简单）
+ */
 
 public class 报数_ {
 
 
     public static void main(String[] args) {
-        报数_.getTheLastOne(new ArrayList<Integer>(){{add(1);add(3);add(5);add(7);add(9);}},3);
+        String str = "12345";
+        Node headerNode = initNode(str);
+        getTheLastNode(headerNode,2);
+
     }
 
-    /**
-     *
-     * @param list n个人（编号1-n）围成一圈报数，报到m的人出队，求最后剩下的那个人
-     * @param m
-     */
-    public static  void getTheLastOne(List<Integer> list, int m){//FIXME
-        int i =0;
-       while(list.size()>1){
-           if(i==m){
-             list.remove(Integer.valueOf(i));
-             i=0;
-           }else{
-               i++;
-           }
 
-       }
-       System.out.print(" "+list.get(0));
+
+    static class Node {
+        String value;
+        Node next;
+
+        Node() {
+
+        }
+    }
+
+
+    //初始化循环链表
+    private static Node initNode(String str) {
+
+        Node headNode = new Node();
+        headNode.value = String.valueOf(str.charAt(0));
+
+        Node tempNode = headNode;
+        for (int i = 1; i < str.length(); i++) {
+            Node node = new Node();
+            node.value = String.valueOf(str.charAt(i));
+            //赋值next
+            tempNode.next = node;
+            //继续指向下一个节点
+            tempNode = node;
+
+        }
+
+        //最后一个节点关联头部节点，形成环形
+        tempNode.next = headNode;
+        return headNode;
+
+    }
+
+
+    private static void getTheLastNode(Node headerNode, int m) {
+        //记录循环开始的头结点
+        Node first = headerNode;
+        while (first!=first.next) {//只要还有下一个节点就持续循环
+            for (int i = 1; i <m-1 ; i++) {//循环报数，并调整first节点
+                //循环结束 这个就是要删除的节点的前一个节点
+                first = first.next;
+            }
+
+            //first后一个节点需要删除。所以要重建链接
+            first.next = first.next.next;
+            //然后循环是从下一个节点重新开始，更改first指向
+            first = first.next;
+
+        }
+
+        System.out.print("最后一个节点是："+first.value);
 
     }
 }
