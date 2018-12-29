0a770d557628984b4b08f3d82371aed6752c1a17
canzhang <canzhang@lexinfintech.com>
2018-12-29 19:36:43 +0800

    样例工程整理（新增sample，后面还要追加base module）

--- app/src/main/java/com/example/administrator/demoall/MainActivity.java
+++ app/src/main/java/com/example/administrator/demoall/MainActivity.java
@@ -9,12 +9,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-import com.example.administrator.demoall.MMKV.MMKVActivity;
 import com.example.administrator.demoall.filemanager.FileTestActivity;
-import com.example.administrator.demoall.webview.WebviewActivity;
-import com.example.administrator.demoall.webview.cache.preload.PreLoadService;
 import com.meituan.android.walle.WalleChannelReader;
 
+/**
+ * TODO 这里需要改造，添加上来Arouter
+ * TODO 以后多一些大模块的分支 相互解耦，方便不需要的时候可以屏蔽掉，提高编译速度
+ * TODO 添加base模块
+ */
 public class MainActivity extends AppCompatActivity {
     public static String TAG = "Test";
 
