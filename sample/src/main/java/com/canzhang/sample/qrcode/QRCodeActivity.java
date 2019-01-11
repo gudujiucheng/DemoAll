7857ff49fa982e73cce9e0ad303d81164dacd433
canzhang <canzhang@lexinfintech.com>
2019-01-11 11:55:11 +0800

    二维码

--- sample/src/main/java/com/canzhang/sample/qrcode/QRCodeActivity.java
+++ sample/src/main/java/com/canzhang/sample/qrcode/QRCodeActivity.java
@@ -8,6 +8,12 @@ import android.widget.ImageView;
 import com.canzhang.sample.R;
 import com.example.base.base.BaseActivity;
 
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+
+import static com.canzhang.sample.qrcode.QrCodeUtils.createQRCodeBitmap;
+
 /**
  * 二维码生成测试
  * 参考文章 ：https://www.cnblogs.com/xch-yang/p/9642255.html
@@ -25,7 +31,23 @@ public class QRCodeActivity extends BaseActivity {
 
     private void addView() {
         ImageView imageView =  new ImageView(this);
-        imageView.setImageBitmap(QrCodeUtils.createQRCodeBitmap("测试",800, 800,"UTF-8","H", "1", Color.BLACK, Color.WHITE));
+        Observable.just("xxxx").map(new Function<String, String>() {
+            @Override
+            public String apply(String s) throws Exception {//转换base64
+
+                return null;
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+
+            }
+        });
+
+
+
+
+        imageView.setImageBitmap(createQRCodeBitmap("测试",800, 800,"UTF-8","H", "1", Color.BLACK, Color.WHITE));
         constraintLayout.addView(imageView);
     }
 }
