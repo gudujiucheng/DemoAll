8bdbf57bf3e7275cc68329258091b4a7ee782549
canzhang <canzhang@lexinfintech.com>
2019-02-28 21:12:59 +0800

    添加toast方法

--- base/src/main/java/com/example/base/base/BaseActivity.java
+++ base/src/main/java/com/example/base/base/BaseActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 public class BaseActivity extends FragmentActivity {
 
@@ -18,7 +19,9 @@ public class BaseActivity extends FragmentActivity {
         Log.e("Test",this.getClass().getSimpleName());
     }
 
-
+    public void showToast(String tips) {
+        Toast.makeText(this, tips, Toast.LENGTH_SHORT).show();
+    }
     public  void log(String msg){
         Log.e(this.getClass().getSimpleName(),msg);
     }
