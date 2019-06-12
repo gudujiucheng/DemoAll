81d4337ac988e4c0a66ea294d1221e50149a1cfa
canzhang <canzhang@lexinfintech.com>
2019-06-12 16:26:24 +0800

    数字测试

---
+++ javatest/src/main/java/com/example/javatest/num_test/MainNumTest.java
@@ -0,0 +1,18 @@
+package com.example.javatest.num_test;
+
+public class MainNumTest {
+
+
+    public static void main(String[] args) {
+        test03();
+    }
+
+    private static void test03() {
+        float a = 10.5f;
+        int b = (int) a;
+//        b:10
+        System.out.print("b:" + b);
+    }
+
+
+}
